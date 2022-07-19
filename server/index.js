c8eea8d44132a664389af85b44d3e4b3b16de46d
Hannah Engelhardt <hannahnengel@gmail.com>
2022-07-19 20:36:41 +0000

    #1 Create an authorizationmiddleware.js, Update server/index.js to include an app.post to handle user sign in

--- server/index.js
+++ server/index.js
@@ -1,10 +1,21 @@
 require('dotenv/config');
 const path = require('path');
+const pg = require('pg');
 const express = require('express');
+// const argon2 = require('argon2');
+// const jwt = require('jsonwebtoken');
+const ClientError = require('./client-error');
 const errorMiddleware = require('./error-middleware');
+// const authorizationMiddleware = require('./authorization-middleware');
 
 const app = express();
 const publicPath = path.join(__dirname, 'public');
+const db = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
 if (process.env.NODE_ENV === 'development') {
   app.use(require('./dev-middleware')(publicPath));
@@ -12,8 +23,36 @@ if (process.env.NODE_ENV === 'development') {
   app.use(express.static(publicPath));
 }
 
+const jsonMiddleware = express.json();
+app.use(jsonMiddleware);
+
 app.get('/api/hello', (req, res) => {
   res.json({ hello: 'world' });
+});
+
+app.post('/api/auth/sign-in', (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    throw new ClientError(400, 'username and password are required fields');
+  }
+  const sql = `
+  select "userId",
+          "hashedPassword"
+        from "users"
+      where "email" = $1`;
+  const params = [email];
+  db.query(sql, params)
+    .then(result => {
+      const [user] = result.rows;
+      if (!user) {
+        throw new ClientError(401, 'invalid login');
+      }
+      if (user.hashedPassword !== password) {
+        throw new ClientError(401, 'incorrect password');
+      }
+      res.json({ user });
+    })
+    .catch(err => next(err));
 });
 
 app.use(errorMiddleware);
