2708c01993609405af4402b4cd5efbbfc94dfc25
Hannah Engelhardt <hannahnengel@gmail.com>
2022-07-19 21:14:04 +0000

    #1 Update server/index.js to use argon2 to hash passwords before posting them in the database & to verify user logins

--- server/index.js
+++ server/index.js
@@ -2,11 +2,11 @@ require('dotenv/config');
 const path = require('path');
 const pg = require('pg');
 const express = require('express');
-// const argon2 = require('argon2');
-// const jwt = require('jsonwebtoken');
+const argon2 = require('argon2');
+const jwt = require('jsonwebtoken');
 const ClientError = require('./client-error');
 const errorMiddleware = require('./error-middleware');
-// const authorizationMiddleware = require('./authorization-middleware');
+const authorizationMiddleware = require('./authorization-middleware');
 
 const app = express();
 const publicPath = path.join(__dirname, 'public');
@@ -47,10 +47,17 @@ app.post('/api/auth/sign-in', (req, res, next) => {
       if (!user) {
         throw new ClientError(401, 'invalid login');
       }
-      if (user.hashedPassword !== password) {
-        throw new ClientError(401, 'incorrect password');
-      }
-      res.json({ user });
+      const { userId, hashedPassword } = user;
+      return argon2
+        .verify(hashedPassword, password)
+        .then(isMatching => {
+          if (!isMatching) {
+            throw new ClientError(401, 'invalid login');
+          }
+          const payload = { userId, email };
+          const token = jwt.sign(payload, process.env.TOKEN_SECRET);
+          res.json({ token, user: payload });
+        });
     })
     .catch(err => next(err));
 });
@@ -60,19 +67,25 @@ app.post('/api/auth/register', (req, res, next) => {
   if (!firstName || !email || !password) {
     throw new ClientError(400, 'username and password are required fields');
   }
-  const sql = `
-  insert into "users" ("firstName", "email", "hashedPassword")
-  values ($1, $2, $3)
-  returning "userId", "email", "createdAt"
-  `;
-  const params = [firstName, email, password];
-  db.query(sql, params)
+  argon2
+    .hash(password)
+    .then(hashedPassword => {
+      const sql = `
+      insert into "users" ("firstName", "email", "hashedPassword")
+      values ($1, $2, $3)
+      returning "userId", "email", "createdAt"
+      `;
+      const params = [firstName, email, hashedPassword];
+      return db.query(sql, params);
+    })
     .then(result => {
       const [user] = result.rows;
       res.status(201).json(user);
     })
     .catch(err => next(err));
 });
+
+app.use(authorizationMiddleware);
 
 app.use(errorMiddleware);
 
